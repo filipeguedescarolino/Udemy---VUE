488b64e3961fb080ff637ce563577c1196901f60
1190100 <1190100@isep.ipp.pt>
2020-11-01 20:09:53 +0000

    methods and simple javascript between {{}} like calling function

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ const app = Vue.createApp({
         return {
             //    i can now use the courseGoal in html section part.
             courseGoal: "Finish the course and learn VuE",
-            vueLink: `https://vuejs.org/`
+            vueLink: `https://vuejs.org/`,
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return "learn Vue !!";
+            } else {
+                return "monster view!";
+            }
+        },
     },
 });
 //  i want to control the section id=usergoal
