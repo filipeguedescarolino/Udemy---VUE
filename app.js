9853a485f7f77a643c2f36719944a3dd5d334983
1190100 <1190100@isep.ipp.pt>
2020-11-01 20:49:11 +0000

    Exercise with data binding. Using v-bind, methods functions

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const app = Vue.createApp({
     data() {
         return {
-            //    i can now use the courseGoal in html section part.
-            courseGoal: "Finish the course and learn VuE",
-            vueLink: `https://vuejs.org/`,
-        };
-    },
-    methods: {
-        outputGoal() {
-            const randomNumber = Math.random();
-            if (randomNumber < 0.5) {
-                return "learn Vue !!";
-            } else {
-                return "monster view!";
-            }
-        },
-    },
-});
-//  i want to control the section id=usergoal
+            myName: "Filipe",
+            myAge: 21,
+            vueImage: "cachorro.jpg"
 
-app.mount("#user-goal");
+        }
+    },
+
+    methods: {
+        greet() {
+            return this.myAge + 5;
+        },
+
+        randomNumber() {
+            return Math.random()
+        }
+    }
+
+})
+
+
+
+
+app.mount("#assignment")
