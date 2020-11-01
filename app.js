98296fb373af1253d53f5320ec6631c0f950f2d7
1190100 <1190100@isep.ipp.pt>
2020-11-01 19:58:59 +0000

    v-bind on href

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
-Vue.createApp({
+const app = Vue.createApp({
     data() {
         return {
-            goals: [],
-            enteredValue: ""
+            //    i can now use the courseGoal in html section part.
+            courseGoal: "Finish the course and learn VuE",
+            vueLink: `https://vuejs.org/`
         };
     },
+});
+//  i want to control the section id=usergoal
 
-    methods: {
-        addGoal() {
-            this.goals.push(this.enteredValue);
-        }
-    }
-
-
-}).mount('#app');
-
-
-// const buttonEl = document.querySelector("button");
-// const inputEl = document.querySelector("input");
-// const listEl = document.querySelector("ul");
-
-// function addGoal() {
-//     const enteredValue = inputEl.value;
-//     const listItemEl = document.createElement("li");
-//     listItemEl.textContent = enteredValue;
-//     listEl.appendChild(listItemEl);
-//     inputEl.value = "";
-// }
-
-
-// buttonEl.addEventListener('click', addGoal)
+app.mount("#user-goal");
