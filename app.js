b71a4ab2433c961572c6ca31428bdf0582104d33
1190100 <1190100@isep.ipp.pt>
2020-11-02 10:17:31 +0000

    Using the native event object with v-on:input

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-    };
-  },
+    data() {
+        return {
+            counter: 0,
+            num: 5,
+            name: ""
+        };
+    },
+    methods: {
+        setName(event, lastName) {
+            this.name = event.target.value + '' + lastName;
+        },
+        add(num) {
+            this.counter = this.counter + this.num;
+        },
+        remove(num) {
+            this.counter = this.counter - this.num;
+        }
+    }
 });
 
 app.mount('#events');
