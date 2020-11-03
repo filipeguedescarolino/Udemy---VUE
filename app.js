f4660b9e6fedcfcb0affb01efc1a3ab1829073de
1190100 <1190100@isep.ipp.pt>
2020-11-03 09:50:32 +0000

    data binding finished. v-on , v-once , and special features with preventdefault and keyup

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const app = Vue.createApp({
         return {
             counter: 0,
             num: 5,
-            name: ""
+            name: "",
+            // the confirmed name should only be updated when we click enter
+            confirmedName: ""
         };
     },
     methods: {
+        confirmInput() {
+            this.confirmedName = this.name;
+        },
+        submitForm() {
+
+            alert("submitted")
+        },
         setName(event, lastName) {
             this.name = event.target.value + '' + lastName;
         },
